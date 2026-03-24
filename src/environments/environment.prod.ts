2d11bbcf07c44bb8940a7f8c48741d7cf968bc5d
Recep May <recepmay@gmail.com>
2026-03-24 12:30:16 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  topStoriesURL: 'https://hacker-news.firebaseio.com/v0/topstories.json',
+  storyItemURL: 'https://hacker-news.firebaseio.com/v0/item/',
+  authorURL: 'https://hacker-news.firebaseio.com/v0/user/',
+  pixabayApiRoot: 'https://pixabay.com/api/?key=22911567-2be38d78952839a9d18019604'
 };
