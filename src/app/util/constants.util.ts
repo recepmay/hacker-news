6b97e100ebf6e214af5f1508805ae0ae515aff2e
Recep May <rma@simplesite.com>
2026-03-24 16:41:28 +0100

    assest adjustment

--- src/app/util/constants.util.ts
+++ src/app/util/constants.util.ts
@@ -1,3 +1,3 @@
-export const defaultImageURL: string = '\'../../../../assets/images/default.jpg\'';
+export const defaultImageURL: string = 'assets/images/default.jpg';
 
 export const defaultText: string = 'Since the provided APIs are providing meta data, in this page you will find some more information about the selected story. The details about the story can be reached via the link positioned below this page. You will also be provided with the author information if they filled up the related field in the API. You can go back via the back button and select another story card to view its details.';
